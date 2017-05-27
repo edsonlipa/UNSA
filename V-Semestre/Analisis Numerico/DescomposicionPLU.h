974971473b6f8bd51edb05a09929076a26c27588
edsonlipa <edson.lipa@gmail.com>
2017-05-26 21:11:45 -0500

    Inteligencia artificial , redes neuronales con base de datos de iris

---
+++ V-Semestre/Analisis Numerico/DescomposicionPLU.h
@@ -0,0 +1,313 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <utility>
+#include <cmath>
+using namespace std;
+typedef float numero;
+typedef vector<numero> row;
+typedef vector<numero> colmnMatrix;
+typedef vector<row> matrix;
+matrix P;
+matrix L;
+matrix U;
+void llenar_matriz(matrix &matriz1)
+{	unsigned nM;
+	numero valor;
+	cout<<"zise of matrix"<<endl;
+	cin>>nM;
+	for (unsigned i = 0; i < nM; ++i)
+	{
+		row fila;
+		cout<<"los valores se ingresaran por filas"<<endl;
+		for (unsigned j = 0; j < nM; ++j)
+		{
+			cout<<"ingrese el valor: "<<endl;
+			cin>>valor;
+			fila.push_back(valor);
+			cout<<"el valor fue ingresado en la posicion ["<<i+1<<"]["<<j+1<<"]"<<endl;
+		}
+		matriz1.push_back(fila);
+	}
+}
+void print(matrix &m)
+{
+	unsigned nM=m.size();
+	for (unsigned i = 0; i < nM; ++i)
+	{
+		for (unsigned j = 0; j < nM; ++j)
+		{
+			if(j==0){cout<<"|";}
+			if(j!=0){cout<<"\t";}
+			cout<<m[i][j];
+		}
+		cout<<"|"<<endl;
+	}
+	cout<<endl;
+}
+void print(row &m)
+{
+	unsigned nM=m.size();
+	for (unsigned j = 0; j < nM; ++j)
+	{
+		cout<<"|";
+		cout<<m[j];
+		cout<<"|"<<endl;
+	}
+
+	cout<<endl;
+}
+void sumBetweenMatrix(matrix &matriz1,matrix &matriz2)//matriz 1 mas matriz 2
+{	//cout<<"suma de matrices"<<endl;
+	unsigned count=matriz1.size();
+	for (int i = 0; i < count; ++i)//columan 1
+	{
+		for (int j = 0; j < count; ++j)//row 2
+		{
+			matriz1[i][j]=matriz1[i][j]+matriz2[i][j];
+		}
+	}
+}
+void multBetweenMatrix(matrix &matriz1,matrix &matriz2)
+{	//cout<<"producto"<<endl;
+	unsigned count=matriz1.size();
+	for (int i = 0; i < count; ++i)//columan 1
+	{
+		for (int j = 0; j < count; ++j)//row 2
+		{
+			numero ans=0;
+			for (int k = 0; k < count; ++k) //iterator
+			{
+					ans+=matriz1[i][k]*matriz2[k][j];
+
+			}
+			if(j==0){cout<<"|";}
+			if(j!=0){cout<<"\t";}
+			cout<<ans;
+
+		}
+			cout<<"|"<<endl;
+	}
+}
+
+void multbyRow(row &fila,numero n)
+{
+//	cout<<"multiplico"<<endl;
+	for (unsigned i = 0; i < fila.size(); ++i)
+	{
+		fila[i]=fila[i]*n;
+	}
+}
+void subtBetweenRow(row &fila1,row fila2) //fila 1 menos la fila2
+{
+	for (unsigned i = 0; i < fila1.size(); ++i)
+	{
+		fila1[i]=fila1[i]-fila2[i];
+	}
+}
+row multMatrxandRow(matrix &p,row &b)
+{	row pb(p.size());
+    float aux=0;
+    for(unsigned int i=0; i<p.size(); i++)
+    {
+        for (unsigned int j=0; j<p.size(); j++)
+        {
+            aux=aux+p[i][j]*b[j];
+        }
+        pb[i]=aux;
+        aux=0;
+    }
+    return pb;
+
+}
+row multandsubt(row fila1,row fila2,numero mult) //fila 1 menos la fila2
+{
+	row newrow(fila1.size());
+	for (unsigned i = 0; i < fila1.size(); ++i)
+	{
+		newrow[i]=fila1[i]-(fila2[i]*mult);
+		//cout<<" "<<newrow[i];
+	}
+		//cout<<endl;
+	return newrow;
+}
+void sumBetweenRow(row &fila1,row fila2) //fila 1 mas la fila2
+{
+	//cout<<"resta"<<endl;
+	for (unsigned i = 0; i < fila1.size(); ++i)
+	{
+		fila1[i]=fila1[i]+fila2[i];
+		//cout<<" pos: "<<i<<" new value: "<<fila1[i]<<" |";
+	}
+	//cout<<endl;
+}
+
+void llenar_Midentidad(matrix &matriz1,unsigned nM)
+{
+	for (unsigned i = 0; i < nM; ++i)
+	{
+		row fila;
+		for (unsigned j = 0; j < nM; ++j)
+		{
+			if(i==j)			{
+				fila.push_back(1);
+			}
+			else
+			fila.push_back(0);
+		}
+		matriz1.push_back(fila);
+	}
+
+}
+
+void maxibyColmns(matrix &matriz,unsigned columna,matrix &P,matrix &L)
+{	//cout<<"__________________________________________________________________"<<endl;
+	//cout<<"maximo por columan cambiar pivote"<<endl;
+	unsigned aux;
+	unsigned aux2;
+	priority_queue<float> maxheap;
+	for (unsigned i = columna; i < matriz.size(); ++i)
+	{
+		maxheap.push(matriz[i][columna]);
+	}
+	for (unsigned i = columna; i < matriz.size(); ++i)
+	{
+		if(matriz[i][columna]==maxheap.top()) aux=i;
+
+	}
+	if (matriz[columna][columna]==matriz[aux][columna]){
+		return;
+	}
+	else
+	{
+	//	cout<<"el cambio se hiso entre "<<columna<<" y "<<aux<<endl;
+		matriz[columna].swap(matriz[aux]);
+		P[columna].swap(P[aux]);
+		L[columna].swap(L[aux]);
+	}
+}
+void upperTriangle(matrix &matriz,matrix &L,matrix &P)
+{	//cout<<"triangulo inferior"<<endl;
+	numero multip=0;
+	for (unsigned i = 0; i < matriz.size(); ++i)//columan
+	{
+		maxibyColmns(matriz,i,P,L);
+		//print(matriz);
+		for (unsigned j = i+1; j < matriz.size(); ++j)//fila
+		{
+			if(matriz[j][i]!=0)
+			{
+			multip=(matriz[j][i]/matriz[i][i]);
+			L[j][i]=multip;
+			//cout<<"multip"<<multip<<endl;
+			matriz[j]=multandsubt(matriz[j],matriz[i],multip);
+			//multbyRow(matriz[j],multip);
+			//subtBetweenRow(matriz[j],matriz[i]);
+
+			}
+		}
+	}
+}
+
+bool strictlyDominatDiag(matrix matriz)
+{
+	for (int i = 0; i < matriz.size(); ++i)//fila
+	{
+		numero filas=0;
+		numero columnas=0;
+
+		for (int j = 0; j < matriz.size(); ++j)
+		{
+			filas+=abs(matriz[i][j]);
+			columnas+=abs(matriz[j][i]);
+		}
+		if (abs(matriz[i][i])<=filas-abs(matriz[i][i])&&abs(matriz[i][i])<=columnas-abs(matriz[i][i]))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+row sustitucion_progresiva(matrix &l,matrix &p,row &b)
+{
+
+    row y(b.size());
+    row pb(b.size());
+    pb =multMatrxandRow(p,b);
+
+    float aux=0;
+
+    for(unsigned int i=0; i<l.size(); i++)
+    {
+        for (unsigned int j=0; j<l.size(); j++)
+        {
+            if(j<i)
+                aux=aux+l[i][j]*y[j];
+        }
+        y[i]=(pb[i]-aux)/l[i][i];
+        aux=0;
+
+    }
+    return y;
+}
+row sustitucion_regresiva(matrix &u,row &y)
+{
+	row x(y.size());
+    float aux=0;
+
+    for(unsigned int i=u.size(); i>0; i--)
+    {
+        for (unsigned int j=u.size(); j>0; j--)
+        {
+            if(j-1>i-1)
+                aux=aux+u[i-1][j-1]*x[j-1];
+        }
+        x[i-1]=(y[i-1]-aux)/u[i-1][i-1];
+        aux=0;
+    }
+    return x;
+}
+void comprobarPLU(matrix A,matrix P,matrix L,matrix U)
+{
+	cout<<"si las matrices son iguales la descompocicon PLU es correcta"<<endl;
+	cout<<"multiplicacion: PxA"<<endl;
+	multBetweenMatrix(P,A);
+	cout<<"multiplicacion: LxU"<<endl;
+	multBetweenMatrix(L,U);
+	cout<<"si las matrices son iguales la descompocicon PLU es correcta"<<endl;
+}
+void descomponer_enPLU(matrix A,matrix &P,matrix &L,matrix &U) ///la descomposicion de P.A=L.U
+{
+	matrix ID;
+	llenar_Midentidad(P,A.size());
+	llenar_Midentidad(ID,A.size());
+	//matrix A={{9,2,-3},{1,-4,2},{2,1,9}};
+	U=A;
+	upperTriangle(U,L,P);
+	sumBetweenMatrix(L,ID);
+	/*cout<<"matriz"<<endl;
+	print(A);
+	cout<<"---P---"<<endl;
+	print(P);
+	cout<<"---L---"<<endl;
+	print(L);
+	cout<<"---U---"<<endl;
+	print(U);*/
+	//cout<<((strictlyDominatDiag(A))?"es estrictamente dominante":"no es estrictamtnete dominante")<<endl;
+
+}
+row resolver_conPLU(matrix &A,row &B)///para resolver ecuaciones A.x=B
+{
+    matrix U;
+	matrix L(A.size(),row(A.size()));
+	matrix P;
+
+    descomponer_enPLU(A,P,L,U);
+    row Y(B.size()); // Vector Y Auxiliar donde se guarda la sustitucion progresiva
+    row X(B.size()); //Vector de resultados
+    Y=sustitucion_progresiva(L,P,B);
+    X=sustitucion_regresiva(U,Y);
+
+    return X;
+
+}
--- VII-Semestre/Inteligencia Artificial/back-propagation/back-propagation.cpp
+++ VII-Semestre/Inteligencia Artificial/back-propagation/back-propagation.cpp
@@ -5,27 +5,37 @@
 #include <vector>
 #include <fstream>
 #include <string>
-
+#include <unistd.h>
 using namespace std;
 
 typedef std::vector<float> vecf;
 typedef std::vector<vecf> matrix;
 typedef std::vector<string> vecs;
-#define umbral 0.00001
-#define alpha 0.05
+#define umbral 0.05
+#define alpha 0.1
 
 void print_vect(vecf v) {
   cout << "[";for(auto i:v){
   cout <<i<< ",";}
   cout << "]\n";
 }
-
+void print_matrix(matrix m){
+  for (size_t i = 0; i < m.size(); i++) {
+    std::cout << "["<<i<<"]";
+    print_vect(m[i]);
+  }
+  std::cout  << '\n';
+}
 float sigmoide(float a){
 	return 1/(1+exp(a*(-1)));
 }
-float dot(vecf vec1,vecf vec2)
-{
+float dot(vecf vec1,vecf vec2){
   float sum=0;
+  if (vec1.size()!=vec2.size()) {
+    std::cout <<vec1.size()<< "/* error */ "<<vec2.size() << '\n';
+    sleep(1);
+
+  }
   for (size_t i = 0; i < vec1.size(); i++) {
     sum=sum+(vec1[i]*vec2[i]);
   }
@@ -33,23 +43,20 @@ float dot(vecf vec1,vecf vec2)
 }
 float error(vecf so,vecf sd){
 	float e=0;
-	for (unsigned i = 0; i < so.size(); ++ i)
+  for (unsigned i = 0; i < so.size(); ++ i)
             e += pow((sd[i]-so[i]),2);
     return e/so.size();
 }
-vecf S(vecf x,matrix layer,bool type)
-{
+vecf S(vecf x,matrix layer,bool type){
   vecf producto;
-  float s;
-  if (type) {s=1;
+  if (type) {
     producto.push_back(1);
-  }else{
-    s=0;
   }
   for (auto i:layer) {
     producto.push_back(dot(x,i));
+
   }
-  for (size_t i = s; i < producto.size(); i++) {
+  for (size_t i = type; i < producto.size(); i++) {
       producto[i]=sigmoide(producto[i]);
   }
   return producto;
@@ -66,7 +73,7 @@ void leer_data(matrix &di, matrix &dout,vecf &data_train,vecf &data_test){
 	vecf vir;vir.push_back(0);vir.push_back(0);vir.push_back(1);
 
 	int act=1;
-
+  aux.push_back(1);
 	if(file.is_open())
   	{
     	while ( file.get (c) ){
@@ -75,7 +82,6 @@ void leer_data(matrix &di, matrix &dout,vecf &data_train,vecf &data_test){
       			if(cont<4){
       				aux.push_back(strtof(p.c_str(),0));
       				cont++;
-              // std::cout << "p "<<p << '\n';
       			}
       			else{
       				di.push_back(aux);
@@ -83,6 +89,7 @@ void leer_data(matrix &di, matrix &dout,vecf &data_train,vecf &data_test){
       				else if(p=="Iris-versicolor"){dout.push_back(ver);}
       				else{dout.push_back(vir);}
       				aux.clear();
+              aux.push_back(1);
       				cont=0;
       				if(act%2==0 || act%5==0){data_train.push_back(act-1);}
       				else{data_test.push_back(act-1);}
@@ -99,14 +106,14 @@ void print_data(matrix di,matrix dout){
   for(int i=0;i<di.size();i++){
 		for(int j=0;j<di[i].size();j++){
 
-      cout<<di[i][j]<<"---";
+      cout<<di[i][j]<<"   ";
 		}
     count++;
-		cout<<"\t\t\t->"<<dout[i][0]<<"---"<<dout[i][1]<<"--- "<<dout[i][2]<<endl;
+		cout<<"\t\t-> \t"<<dout[i][0]<<"--"<<dout[i][1]<<"--"<<dout[i][2]<<endl;
 	}
   std::cout << count<<" registros" << '\n';
 
-}
+  }
 matrix create_layer(float n_neu,float n_input) {
   matrix matriz(n_neu,vecf(n_input));
   for (size_t i = 0; i < n_neu; i++) {
@@ -116,6 +123,7 @@ matrix create_layer(float n_neu,float n_input) {
   }
   return matriz;
 }
+
 void changeLayerOut(matrix &w,vecf so,vecf sd,vecf &delta_out,vecf sh){
 	for (unsigned i = 0; i < w.size (); ++ i){
 		delta_out[i]=(sd[i]-so[i])*(1-so[i])*(so[i]);
@@ -130,71 +138,134 @@ void changeLayerHide(matrix &w1,vecf s,vecf input,vecf delta,matrix w2){
 		for(unsigned k=0 ;k<w2.size();++k){
 			sum+=w2[k][i]*delta[k];
 		}
-		d=sum*(1-s[i]*s[i]);
-
-        for (unsigned j = 0; j < w1[i].size (); ++ j)
-            w1[i][j]=alpha*(d*input[j]);
+		d=sum*((1-s[i])*s[i]);
+    for (unsigned j = 0; j < w1[i].size (); ++ j)
+            w1[i][j]-=alpha*(d*input[j]);
 	}
 }
 
-
 void RedNeuronal(vecf input,vecf output,matrix &w1,matrix &w2){
 
+  fstream file;
+  file.open("errores.dat", std::fstream::in | std::fstream::out | std::fstream::app);
+  // print_matrix(w2);
+  // print_matrix(w1);
 	vecf sh=S(input,w1,1);
 	vecf so=S(sh,w2,0);
+  // print_vect(sh);
+  // print_vect(so);
 	float e=error(so,output);
 	vecf delta (so.size());
 	while(e>=umbral){
 		changeLayerOut(w2,so,output,delta,sh);
-		changeLayerHide(w1,sh,input,delta,w2);
+    changeLayerHide(w1,sh,input,delta,w2);
+    // sleep(1);
     sh=S(input,w1,1);
 		so=S(sh,w2,0);
 		e=error(so,output);
-		// cout<<e<<endl;
+		file<<e<<endl;
 	}
+  file.close();
 }
-bool testeo(matrix &w1,matrix &w2,vecf &input,vecf &output){
+
+int testeo(matrix &w1,matrix &w2,vecf &input){
 	vecf sh=S(input,w1,1);//printMatrix(sh);cout<<endl;
 	vecf so=S(sh,w2,0);
 
-	float e=error(so,output);
+  vecf uno{1,0,0};
+  vecf dos{0,1,0};
+  vecf tres{0,0,1};
 
-	if(e>=umbral){return false;}
-	else{return true;}
+  float e1=error(so,uno);
+  // std::cout << "error de testeo "<<e << '\n';
+  if(e1<=umbral){return 0;}
+  float e2=error(so,dos);
+  if(e2<=umbral){return 1;}
+  float e3=error(so,tres);
+	if(e3<=umbral){return 2;}
 }
 
+void pre_train(matrix &w1,matrix &w2,matrix data_input,matrix data_output,vecf train){
+  print_matrix(w2);
+  print_matrix(w1);
+
+  fstream file;
+  file.open("errores.dat", std::fstream::in | std::fstream::out | std::fstream::app);
+
+  float prom=1;
+  float e=1;
+
+  while(prom>=umbral){
+  for (unsigned i = 0; i < train.size (); ++ i){
+      vecf sh=S(data_input[train[i]],w1,1);
+      vecf so=S(sh,w2,0);
+      vecf delta (so.size());
+      changeLayerOut(w2,so,data_output[train[i]],delta,sh);
+      changeLayerHide(w1,sh,data_input[train[i]],delta,w2);
+      sh=S(data_input[train[i]],w1,1);
+      so=S(sh,w2,0);
+      e=error(so,data_output[train[i]]);
+      prom+=e;
+  	}
+    prom=prom/w1.size();
+    std::cout << "promedio: "<<prom << '\n';
+    file<<prom << '\n';
+  }
+  file.close();
+  std::cout << "fin pre train" << '\n';
+		// cout<<e<<endl;
+}
 void train(matrix data_input,matrix data_output,vecf train,vecf test)
 {
-	matrix w1=create_layer(data_input[0].size()*2,data_input[0].size()+1);
+	matrix w1=create_layer(14,data_input[0].size());
 	matrix w2=create_layer(3,w1.size()+1);
+  print_matrix(w2);
+  print_matrix(w1);
+  pre_train(w1,w2,data_input,data_output,train);
 
-	for(int i=0;i<train.size();i++){
+	// for(intS
+  print_matrix(w2);
+  print_matrix(w1);
 
-		RedNeuronal(data_input[train[i]],data_output[train[i]],w1,w2);
-		// cout<<"Entrenamiento -> "<<i+1<<endl;
-	}
-
-	int cont=1,buenas=0,malas=0;
-
-	for(int i=0;i<test.size();i++){
-    vecf input((data_input[train[i]]));
-    vecf output(data_output[train[i]]);
-		if(testeo(w1,w2,data_input[train[i]],data_output[train[i]])){buenas++;}
-		else{malas++;}
-		if(cont==20){cout<<"Buenas: "<<buenas<<" Malas: "<<malas<<endl;cont=0;buenas=0;malas=0;}
-		cont++;
-	}
+  float type=-1;
+  matrix dispercion(3,vecf(3));
+  for (size_t i = 0; i < test.size(); i++) {
+    if (i%20==0) {type++;}
+    dispercion[type][testeo(w1,w2,data_input[test[i]])]++;
+  }
+  print_matrix(dispercion);
+  float efectividad=0;
+        for (size_t i = 0; i < 3; i++) {
+          efectividad+=(dispercion[i][i]*100)/20;
+          /* code */
+        }
+std::cout << "efectividad "<<efectividad/3 <<"%"<< '\n';
 
 }
+void normalizacion(matrix &data_input,vecf data_train) {
+  for (size_t i = 0; i < data_train.size(); i++) {
+    float max=0;
+    float min=10;
+    for (size_t j = 1; j < data_input[i].size(); j++) {
+      if (max>data_input[i][j])  max=data_input[i][j];
+      if (min<data_input[i][j]) min=data_input[i][j];
+    }
+    for (size_t j = 1; j < data_input[i].size(); j++) {
+      data_input[i][j]=(data_input[i][j]-min)/(max-min);
+    }
+  }
+}
 int main(int argc, char const *argv[]) {
+  srand(time(NULL));
+
   matrix data_input;
 	matrix data_out;
 	vecf data_test;
 	vecf data_train;
 	leer_data(data_input,data_out,data_train,data_test);
+  normalizacion(data_input,data_train);
   // print_data(data_input,data_out);
-
-	train(data_input,data_out,data_train,data_test);
+  train(data_input,data_out,data_train,data_test);
 
   return 0;
 }
